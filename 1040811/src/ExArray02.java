0593b1092e9994e48cf36062ef6457a5be47b3d3
104021055 <104021055@live.asia.edu.tw>
2015-08-11 14:31:29 +0800

    0811

--- 1040811/src/ExArray02.java
+++ 1040811/src/ExArray02.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class ExArray02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		Random rnd = new Random();
+		int data[] = new int[10];
+		boolean flag = false;
+		int i = 0;
+		while (i < 10) {
+			data[i] = rnd.nextInt(100);
+			flag = false;
+			for (int j = 0; j < i; j++) {
+				if (data[i] == data[j]) {
+					flag = true;
+				}
+			}
+			if (flag == false) {
+				i++;
+			}
+		}
+		for (i = 0; i < 10; i++) {
+			System.out.println(data[i]);
+		}
 	}
 
 }
