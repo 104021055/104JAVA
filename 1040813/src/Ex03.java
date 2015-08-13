648e025cb85d98c7b22be537adbde88b4469db91
104021055 <104021055@live.asia.edu.tw>
2015-08-13 16:03:08 +0800

    0813

--- 1040813/src/Ex03.java
+++ 1040813/src/Ex03.java
@@ -1,49 +1,51 @@
 import java.util.Scanner;
+
 public class Ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner scn=new Scanner(System.in);
-System.out.print("請輸入幾乘幾的矩陣");
-int i=scn.nextInt();
-int j=scn.nextInt();
-int l=scn.nextInt();
-int k=scn.nextInt();
-int p=0;
-int q=0;
-if(j!=l){
-	System.out.println("矩陣錯誤");
-}
-int data1[][]=new int[i][j];
-int data2[][]=new int[j][k];
-int dataAnswer[][]=new int[p][q];
-int w=0;
-int x=0;
-int y=0;
-int z=0;
-System.out.println("輸入第一矩陣數字");
-for(w=0;w<i;w++){
-	for(x=0;x<j;x++){
-		data1[w][x]=scn.nextInt();
-	}
-}
-System.out.println("輸入第二矩陣數字");
-for(y=0;y<j;y++){
-	for(z=0;z<k;z++){
-		data2[y][z]=scn.nextInt();
-	}
-}
-int sum=1;
-for(w=0;w<i;w++){
-	for(z=0;z<k;z++){
-		for(x=0;x<j;x++){
-			dataAnswer[p][q]+=data1[w][x]*data2[y][z];
+		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入幾乘幾的矩陣");
+		int i = scn.nextInt();
+		int j = scn.nextInt();
+		int l = scn.nextInt();
+		int k = scn.nextInt();
+		int p = 0 ;
+		int q = 0 ;
+		if (j != l) {
+			System.out.println("矩陣錯誤");
 		}
-		q++;
-	}
-	p++;
-}
-System.out.println("答案為"+dataAnswer);
+		int data1[][] = new int[i][j];
+		int data2[][] = new int[j][k];
+		int dataAnswer[][] = new int[i][k];
+		System.out.println("輸入第一矩陣數字");
+		for (int w = 0; w < i; w++) {
+			for (int x = 0; x < j; x++) {
+				data1[w][x] = scn.nextInt();
+			}
+		}
+		System.out.println("輸入第二矩陣數字");
+		for (int x = 0; x < j; x++) {
+			for (int z = 0; z < k; z++) {
+				data2[x][z] = scn.nextInt();
+			}
+		}
+		for (int w = 0; w < i; w++) {
+			for (int z = 0; z < k; z++) {
+				for (int x = 0; x < j; x++) {
+					dataAnswer[p][q] += data1[w][x] * data2[x][z];
+				}
+				q++;
+			}
+			p++;
+			q = 0;
+		}
+		for (p = 0; p < i; p++) {
+			for (q = 0; q < k; q++) {
+				System.out.print(dataAnswer[p][q] + "\t");
+			}
+			System.out.println();
+		}
 	}
 
 }
