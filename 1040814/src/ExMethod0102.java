881cebc0213788818b16057470a01702798acc13
104021055 <104021055@live.asia.edu.tw>
2015-08-14 12:12:12 +0800

    0814

--- 1040814/src/ExMethod0102.java
+++ 1040814/src/ExMethod0102.java
@@ -1,9 +1,24 @@
-
+import java.util.Scanner;
 public class ExMethod0102 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+        System.out.println("n=?");
+        long n =scn.nextLong();
+        int sum =(int)fun(n);
+        System.out.println(" "+sum);
+	}
+        public static long fun(long v1){
+        long sum = 0;
+        String m=""+v1;
+        int len = m.length();
+        char[] a =m.toCharArray();
+        for(int i=0;i<len;i++){
+        	int c=a[i]-'0';
+        sum+=c;	
+        }
+        return sum;
 	}
 
 }
