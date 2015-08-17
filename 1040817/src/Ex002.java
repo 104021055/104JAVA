5a26269458a60166c07f1f8b2c12aa304ed2d12a
104021055 <104021055@live.asia.edu.tw>
2015-08-17 11:51:59 +0800

    0817

--- 1040817/src/Ex002.java
+++ 1040817/src/Ex002.java
@@ -1,9 +1,21 @@
-
+import java.util.Scanner;
+import java.util.Random;
 public class Ex002 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		Random rnd=new Random();
+		int i=0;
+		i=rnd.nextInt(1000);
+		while(i>0){
+			System.out.print(fun(i));
+		}
+			}
+			public static int fun(int i){
+				int v1=i%10;
+				i=i/10;
+				return i;
 	}
 
 }
