756b06b2ddd9581bb5e049ac521eee8d780b5485
104021055 <104021055@live.asia.edu.tw>
2015-08-17 14:48:16 +0800

    0817

--- 1040817/src/Ex003.java
+++ 1040817/src/Ex003.java
@@ -1,9 +1,20 @@
-
+import java.util.Scanner;
 public class Ex003 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn=new Scanner(System.in);
+System.out.print("¿é¤J¤@¼Æ¦r");
+float i=scn.nextFloat();
+fun(i);
+	}
+	public static void fun(float i){
+		int a=0;
+		while(i>1){
+			i=i/10;
+			a++;
+		}
+		System.out.println(a);
 	}
 
 }
