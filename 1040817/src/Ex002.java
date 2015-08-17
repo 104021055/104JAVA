cb600ec7dd40fd934ebd8d3cb72abbb90fac345e
104021055 <104021055@live.asia.edu.tw>
2015-08-17 14:30:02 +0800

    0817

--- 1040817/src/Ex002.java
+++ 1040817/src/Ex002.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 import java.util.Random;
+
 public class Ex002 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scn=new Scanner(System.in);
-		Random rnd=new Random();
-		int i=0;
-		i=rnd.nextInt(1000);
-		while(i>0){
-			System.out.print(fun(i));
-			i--;
-		}
-			}
-			public static int fun(int i){
-				int v1=i%10;
-				i=i/10;
-				return i;
+		Scanner scn = new Scanner(System.in);
+		Random rnd = new Random();
+		int i = rnd.nextInt(1001);
+		fun1(i);
+	}
+
+	public static void fun1(int i) {
+		int v1=(i-i%1000)/1000;
+		System.out.print(v1+"千");
+		i=i%1000;
+		int v2=(i-i%100)/100;
+		System.out.print(v2+"百");
+		i=i%100;
+		int v3=(i-i%10)/10;
+		System.out.print(v3+"十");
+		i=i%10;
+		int v4=i-i%1;
+		System.out.print(v4+"元");
 	}
 
 }
--- 1040817/src/Ex02.java
+++ 1040817/src/Ex02.java
@@ -8,15 +8,15 @@ System.out.println("請輸入國英數三科成績");
 float C=scn.nextFloat();
 float E=scn.nextFloat();
 float M=scn.nextFloat();
-float v1=M+E+C;
-float v2=(M+C+E)/3;
-sum(v1);
-avg(v2);
+sum(C,E,M);
+avg(C,E,M);
 	}
-public static void sum(float v1){
+public static void sum(float C,float E,float M){
+	float v1=C+M+E;
 	System.out.print("總成績為"+v1);
 }
-public static void avg(float v2){
+public static void avg(float C,float E,float M){
+	float v2=(C+M+E)/3;
 	System.out.print("平均成績為"+v2);
 }
 }
