de297555e35eaae2ced147ca908a2fd99571614d
104021055 <104021055@live.asia.edu.tw>
2015-08-19 16:00:01 +0800

    0819

--- HelloTest/src/YI.java
+++ HelloTest/src/YI.java
@@ -1,9 +1,17 @@
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class YI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¡@¡@¡@¡@¡¹¡¹¡¹");
+		
+		JFrame frame = new JFrame();
+        frame.setSize(600, 480);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setVisible( true );
+        
+        System.out.println("¡@¡@¡@¡@¡¹¡¹¡¹");
 		System.out.println("¡@¡@¡@¡¹¡¹¡¹");
 		System.out.println("¡@¡@¡¹¡¹¡¹¡¹");
 		System.out.println("¡@¡¹¡¹¡¹¡¹¡¹");
