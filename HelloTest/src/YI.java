23507432d046b16095e2f7e8ade1212deaf6d292
104021055 <104021055@live.asia.edu.tw>
2015-08-19 15:00:20 +0800

    0819

--- HelloTest/src/YI.java
+++ HelloTest/src/YI.java
@@ -3,6 +3,25 @@ public class YI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡¹¡¹¡¹¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹");
+		
 
 	}
 
