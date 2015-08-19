af6c8db5402aa0ab824fe0087bc748b8f7ae1281
104021055 <104021055@live.asia.edu.tw>
2015-08-19 17:42:44 +0800

    0819

--- 1040819/src/YI.java
+++ 1040819/src/YI.java
@@ -1,8 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class YI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame();
+        frame.setSize(600, 480);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setVisible( true );
+        
+        System.out.println("¡@¡@¡@¡@¡¹¡¹¡¹¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡¹¡¹¡¹¡@¡@¡@¡@¡@¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡¹¡¹¡¹¡¹¡@¡@¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡¹¡¹¡¹¡¹¡¹¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡¹¡¹¡¹¡@¡¹¡¹¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡@¡@¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡@¡@¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡@¡@¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡@¡@¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹¡@¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡¹¡¹¡@¡¹¡¹¡@¡@¡¹¡¹¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡@¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡¹¡@¡¹¡¹¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡¹¡¹¡@¡¹¡¹¡@¡@¡@¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡¹¡@¡@¡@¡¹¡¹¡@¡@¡@¡¹¡¹¡¹¡¹¡@¡@¡@¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹¡@¡¹¡¹¡@¡@¡@¡¹¡¹¡@¡@¡¹¡¹¡@¡¹¡¹");
+		System.out.println("¡@¡@¡@¡@¡¹¡¹¡@¡@¡@¡¹¡¹¡¹¡@¡@¡@¡@¡¹¡@¡@¡@¡@¡¹¡¹¡¹");
 
 	}
 
