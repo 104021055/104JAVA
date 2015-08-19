981d4950a78d3f720a92f35eb8bffe583f1153a9
104021055 <104021055@live.asia.edu.tw>
2015-08-19 20:13:19 +0800

    0819

--- 1040819/src/YI.java
+++ 1040819/src/YI.java
@@ -10,6 +10,19 @@ public class YI {
 		JFrame frame = new JFrame();
         frame.setSize(600, 480);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        
+        JCheckBox checkbox = new JCheckBox("阿弟仔可愛");
+        JRadioButton radiobutton = new JRadioButton("義大利麵好吃");
+        JButton button = new JButton("哈哈哈哈哈哈哈哈哈");
+        JLabel label = new JLabel("HELLO~~~~~");
+        JTextArea textarea = new JTextArea("星期五記得去吃飯喔");
+         
+        frame.getContentPane().add(BorderLayout.EAST, checkbox);
+        frame.getContentPane().add(BorderLayout.WEST, radiobutton);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.NORTH , label);
+        frame.getContentPane().add(BorderLayout.CENTER, textarea);
+        
         frame.setVisible( true );
         
         System.out.println("　　　　★★★　　　★★　　★★");
