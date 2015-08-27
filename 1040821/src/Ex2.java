d4e66e34cf88e786c7576fd8f9c6333f22a03d69
104021055 <104021055@live.asia.edu.tw>
2015-08-27 14:22:36 +0800

    0827

--- 1040821/src/Ex2.java
+++ 1040821/src/Ex2.java
@@ -4,6 +4,7 @@ public class Ex2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainFrame2 mFrm=new MainFrame2();
+		mFrm.setTitle("記憶力大考驗!!!!!");
 		mFrm.setVisible(true);
 
 	}
